894d843a14c89b0f9f4902337601450d0d2f70c9
HemanthThota39 <hemant.thota@gmail.com>
2023-03-19 00:08:15 +0530

    Creating playlist and adding the songs to playlist

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ client_secret = '5e99af487cba46de8360b1729d1c8e34'
 redirect_uri = 'http://localhost:5000/callback'
 app.secret_key = 'Hem998$$'
 # Define the Spotify scopes required to read the user's playlist
-scopes = ['playlist-read-private', 'playlist-read-collaborative']
+scopes = ['playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-private','playlist-modify-public']
 
 @app.route('/login')
 def login():
@@ -78,16 +78,59 @@ def transfer():
             if response.status_code == 200:
                 track_ids.append(response.json()['tracks']['items'][0]['id'])
             else:
-                return 'Failed to get user playlists'
+                return 'Failed to get tracks'
         
         print(track_ids)
-        headers = {'Authorization': f'Bearer {session["access_token"]}'}
-        response = requests.get('https://api.spotify.com/v1/me/playlists', headers=headers)
+        #get user's user_id
+        url = f"https://api.spotify.com/v1/me"
+        headers = {
+            'Authorization': f'Bearer {session["access_token"]}',
+            'Content-Type': 'application/json',
+        }
+        response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            playlists = response.json()['items']
-            return f'You have {len(playlists)} playlists in your Spotify account.'
+            user_id = response.json()['id']
         else:
-            return 'Failed to get user playlists'
+            return 'Failed to get user id'
+        print(user_id)
+
+        #create a playlist 
+        url_create_playlist = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+        headers = {
+            'Authorization': f'Bearer {session["access_token"]}',
+            'Content-Type': 'application/json',
+        }
+        data = {
+            'name': 'Amazon Playlist',
+            'description': 'Playlist created from Amazon Music',
+            'public': True
+        }
+        response = requests.post(url_create_playlist, headers=headers, data=json.dumps(data))
+        if response.status_code == 201:
+            playlist_id = response.json()['id']
+        else:
+            return 'Failed to create playlist'
+        print(playlist_id)
+
+        #add tracks to the playlist using track_id from track_ids and playlist_id
+        url_add_tracks = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+        headers = {
+            'Authorization': f'Bearer {session["access_token"]}',
+            'Content-Type': 'application/json',
+        }
+        data = {
+            'uris': [f"spotify:track:{track_id}" for track_id in track_ids]
+        }
+        response = requests.post(url_add_tracks, headers=headers, data=json.dumps(data))
+        if response.status_code == 201:
+            print('Successfully added tracks to playlist')
+        else:
+            return 'Failed to add tracks to playlist'
+        
+
+        
+            
+        return 'Success!'
     else:
         return 'Please login to your Spotify account'
 
