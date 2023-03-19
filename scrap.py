d2c142644061642570f5682f3d7e5d23f61e82e5
DeadPooL <80475876+SurajValluru@users.noreply.github.com>
2023-03-20 01:57:14 +0530

    Song Data Extract
    Now able to extract all song data, prior which only can extract song titles.

--- scrap.py
+++ scrap.py
@@ -24,10 +24,8 @@ def getSongs(playlist_url='', playlist_id=''):
     # Parse the HTML content with BeautifulSoup
     soup = BeautifulSoup(html, 'html.parser')
 
-    # Extract the song names from the parsed HTML
-    song_names = set()
-    for song in soup.find_all('div', class_='tracklist-name'):
-        song_names.add(song.text.strip())
+    # Extract the song names, artists, and durations from the parsed HTML
+    songs_data = set()
 
     last_count = 0
 
@@ -35,13 +33,32 @@ def getSongs(playlist_url='', playlist_id=''):
     # Simulate scrolling down the page to load more content
     while True:
         soup = BeautifulSoup(html, 'html.parser')
-        for song in soup.find_all('div', class_='col1'):
-            song_names.add(song.text.strip())
+        for song in soup.find_all('div', class_='content'):
+            # Extract song title
+            title = song.find('div', class_='col1').text.strip()
+
+            # Extract artist name
+            artist = song.find('div', class_='col2').text.strip()
+
+            # Extract song duration
+            duration = song.find('div', class_='col4').text.strip()
+
+            # Append song data to the list
+            songs_data.add((title, artist, duration))
+
         driver.execute_script("window.scrollBy(0, window.innerHeight - window.innerHeight/20);")
         new_height = driver.execute_script("return document.body.scrollHeight")
         time.sleep(0.1)
         html = driver.execute_script("return document.documentElement.outerHTML")
-        if(last_count !=0 and len(song_names) == last_count):
+        if(last_count !=0 and len(songs_data) == last_count):
             break
-        last_count = len(song_names)
-    return song_names
+        last_count = len(songs_data)
+    return songs_data
+
+
+if __name__ == "__main__":
+    songs = getSongs(playlist_url='https://music.amazon.in/user-playlists/e6fe2efcf6c748c9b5e3bf71dc6507c2i8n0?ref=dm_sh_b7iRPSNXUWG6Un8eYy523VcJM')
+    for song in songs:
+        print(song)
+    print()
+    print(len(songs))
